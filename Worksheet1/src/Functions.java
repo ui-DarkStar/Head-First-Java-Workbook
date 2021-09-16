8ca8e6343c70d609889760dae5f4d1040c767573
DarkStar <8171397+ui-DarkStar@users.noreply.github.com>
2021-09-16 18:36:55 +0100

    Worksheet 1 task 7 - 9

--- Worksheet1/src/Functions.java
+++ Worksheet1/src/Functions.java
@@ -1,12 +1,20 @@
 
 public class Functions {
 
+	public static void outputArrayValues(int arrayParameter[]) {
+		int loop = 0;
+		while(loop < arrayParameter.length) {
+			System.out.println("Number "+loop+" is = "+arrayParameter[loop]);
+			loop ++;
+		}
+
+	}
+
 	public static void main(String[] args) {
 		int integerArray1[] = new int[10];
 		int integerArray2[] = new int[5];
-		
-		int loop = 0;
-		int loop2 = 0;
+
+
 		integerArray1[0] = 10;
 		integerArray1[1] = 9;
 		integerArray1[2] = 8;
@@ -17,27 +25,20 @@ public class Functions {
 		integerArray1[7] = 3;
 		integerArray1[8] = 2;
 		integerArray1[9] = 1;
-		
+
 		integerArray2[0] = 2;
 		integerArray2[1] = 3;
 		integerArray2[2] = 4;
 		integerArray2[3] = 5;
 		integerArray2[4] = 6;
-		
+
 		System.out.println("The loop of numbers are: ");
-		while(loop < integerArray1.length  ) {
-			
-			System.out.println("Number "+loop+" is = "+integerArray1[loop]);
-			loop ++;
-		}
+		outputArrayValues(integerArray1);
+
 		System.out.println();
 		System.out.println("The 2nd loop of numbers are: ");
-		while(loop2 < integerArray2.length  ) {
-			
-			System.out.println("Number "+loop2+" is = "+integerArray2[loop2]);
-			loop2 ++;
-		}
-		
+		outputArrayValues(integerArray2);
+
 	}
 
 }
