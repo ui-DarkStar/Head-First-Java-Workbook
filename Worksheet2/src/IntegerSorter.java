5d01b31ba6d02e3ff72b6a521a51ea16e0908e49
DarkStar <8171397+ui-DarkStar@users.noreply.github.com>
2021-09-16 20:59:46 +0100

    Worksheet 2 task 1 - 4

---
+++ Worksheet2/src/IntegerSorter.java
@@ -0,0 +1,80 @@
+
+public class IntegerSorter {
+
+	public static void print(int array[]) {
+		int loop = 0;
+		while(loop < array.length) {
+			if (loop == 0) {
+				System.out.print(array[loop]);
+			}else {
+				System.out.print(", " + array[loop]);
+			}
+			loop ++;
+		}
+		System.out.println();
+		System.out.println("----------------------------------------------");
+	}
+
+
+	public static void arraySort(int array[]) {
+		boolean swap;
+		int run = 1;
+
+		do {
+			int loop = 0;
+			int held;
+			swap = false;
+			while (loop < (array.length - 1)) {
+				if(array[loop] > array[loop + 1]) {
+					held = array[loop + 1];
+					array[loop + 1] = array[loop];
+					array[loop] = held;
+					swap = true;
+				}
+				loop ++;
+
+
+			}
+			if(swap = true) {
+			System.out.println("the number of sort runs is " + run + " and the order is");
+			print(array);
+			run ++;
+			}
+
+
+		}while(swap == true);
+
+
+	}
+
+
+
+	public static void main(String[] args) {
+		int integerArray[] = new int[10];
+
+		integerArray[0] = 10;
+		integerArray[1] = 9;
+		integerArray[2] = 8;
+		integerArray[3] = 7;
+		integerArray[4] = 6;
+		integerArray[5] = 5;
+		integerArray[6] = 4;
+		integerArray[7] = 3;
+		integerArray[8] = 2;
+		integerArray[9] = 1;
+
+		System.out.println("The list of unsorted numbers are");
+		print(integerArray);
+		arraySort(integerArray);
+
+	}
+}
+//LOOP UNTIL NO SWAPPING HAS HAPPENED
+//SWAPS EQUALS FALSE
+//LOOP UNTIL WE HAVE CHECKED ALL UNSORTED NUMBERS IN ARRAY
+//	IF CURRENT NUMBER IS GREATER THAN THE NEXT THEN
+//		SWAP CURRENT NUMBER AND NEXT NUMBER
+//		SWAPS EQUALS TRUE
+//	END IF
+//END LOOP
+//END LOOP
