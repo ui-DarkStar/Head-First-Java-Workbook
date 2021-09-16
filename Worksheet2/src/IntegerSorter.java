2500dd0b100150ffb28f7c6f4f88c72a54238309
DarkStar <8171397+ui-DarkStar@users.noreply.github.com>
2021-09-16 21:38:28 +0100

    Worksheet 2 task 10 + RNG array

--- Worksheet2/src/IntegerSorter.java
+++ Worksheet2/src/IntegerSorter.java
@@ -36,9 +36,39 @@ public class IntegerSorter {
 
 			}
 			if(swap == true) {
-			System.out.println("the number of sort runs is " + run + " and the order is");
-			print(array);
-			run ++;
+				System.out.println("the number of sort runs is " + run + " and the order is");
+				print(array);
+				run ++;
+			}
+
+
+		}while(swap == true);
+
+
+	}
+	public static void bubbleSortReverce(int array[]) {
+		boolean swap;
+		int run = 1;
+
+		do {
+			int loop = 0;
+			int held;
+			swap = false;
+			while (loop < (array.length - 1)) {
+				if(array[loop] < array[loop + 1]) {
+					held = array[loop + 1];
+					array[loop + 1] = array[loop];
+					array[loop] = held;
+					swap = true;
+				}
+				loop ++;
+
+
+			}
+			if(swap == true) {
+				System.out.println("the number of sort runs is " + run + " and the order is");
+				print(array);
+				run ++;
 			}
 
 
@@ -47,26 +77,38 @@ public class IntegerSorter {
 
 	}
 
+	public static int[] rngArray() {
+		int loop = 0;
+		int array[] = new int[20];
+		while(loop < array.length) {
+			array[loop] = (int)(Math.random()*100) +1;
+			loop ++;
+		}
 
+		return array;
+
+	}
 
 	public static void main(String[] args) {
-		int integerArray[] = new int[10];
 
-		integerArray[0] = 10;
-		integerArray[1] = 9;
-		integerArray[2] = 8;
-		integerArray[3] = 7;
-		integerArray[4] = 6;
-		integerArray[5] = 5;
-		integerArray[6] = 4;
-		integerArray[7] = 3;
-		integerArray[8] = 2;
-		integerArray[9] = 1;
+
+		int[] integerArray = rngArray();
+
+		//integerArray[0] = 10;
+		//integerArray[1] = 9;
+		//integerArray[2] = 8;
+		//integerArray[3] = 7;
+		//integerArray[4] = 6;
+		//integerArray[5] = 5;
+		//integerArray[6] = 4;
+		//integerArray[7] = 3;
+		//integerArray[8] = 2;
+		//integerArray[9] = 10;
 
 		System.out.println("The list of unsorted numbers are");
 		print(integerArray);
 		bubbleSort(integerArray);
-
+		bubbleSortReverce(integerArray);
 	}
 }
 
